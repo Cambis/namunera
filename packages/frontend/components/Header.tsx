c2df605c50faff780855758329c4003452674724
James Zhu <jianbinzhu0_0@hotmail.com>
2022-09-21 15:24:24 +1200

    Header

---
+++ packages/frontend/components/Header.tsx
@@ -0,0 +1,33 @@
+import cn from 'classnames';
+
+export type HeaderLink = {
+  id: string;
+  title?: string | null | undefined;
+  link: string;
+  menuTitle?: string | null | undefined;
+}
+
+export type HeaderProps = {
+  nodes: Array<HeaderLink>;
+}
+
+const Header = (props: HeaderProps) => (
+  <header className="container flex justify-between mx-auto px-4">
+    <a className="header-logo" href="/">
+      <img
+        src="/logo.svg"
+        className="w-32 h-32"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+    </a>
+    <ul className="flex my-auto">
+      { props.nodes.map((item: HeaderLink, idx: number) => (
+        <li className={cn({"ml-2" : idx !== 0})} id={item.id}>
+          <a href={item.link} className="px-4 py-2 block rounded-md bg-yellow-200 hover:bg-yellow-300">{item.title}</a>
+        </li>
+      ))}
+    </ul>
+  </header>
+);
+
+export default Header;
--- packages/frontend/components/Layout.tsx
+++ packages/frontend/components/Layout.tsx
@@ -1,13 +1,19 @@
 import type { ComponentChildren } from "preact";
 import cn from "classnames";
+import Header, { HeaderLink } from './Header.tsx';
+
 
 type LayoutProps = {
   classNames?: string;
   children: ComponentChildren;
+  headerProps: {
+    nodes: HeaderLink[]
+  };
 };
 
-const Layout = ({ classNames, children }: LayoutProps) => (
+const Layout = ({ classNames, children, headerProps }: LayoutProps) => (
   <>
+    <Header nodes={headerProps.nodes} />
     <main className={cn("min-h-screen max-w-screen-md mx-auto", classNames)}>
       {children}
     </main>
