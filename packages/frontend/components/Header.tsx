fdbf616ceaa50ac2afda68f69bc502a98d7b1cc8
James Zhu <jianbinzhu0_0@hotmail.com>
2022-09-22 10:43:57 +1200

    PR feedback tweaks

--- packages/frontend/components/Header.tsx
+++ packages/frontend/components/Header.tsx
@@ -7,7 +7,7 @@ export type HeaderLink = {
   menuTitle?: string | null | undefined;
 }
 
-export type HeaderProps = {
+type HeaderProps = {
   nodes: Array<HeaderLink>;
 }
 
@@ -21,7 +21,7 @@ const Header = (props: HeaderProps) => (
       />
     </a>
     <ul className="flex my-auto">
-      { props.nodes.map((item: HeaderLink, idx: number) => (
+      {props.nodes.map((item: HeaderLink, idx: number) => (
         <li className={cn({"ml-2" : idx !== 0})} id={item.id}>
           <a href={item.link} className="px-4 py-2 block rounded-md bg-yellow-200 hover:bg-yellow-300">{item.title}</a>
         </li>
